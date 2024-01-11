a50a577e6d8a62782f5281bdf0470fc0b80f150b
atsukoba <atsuya_wgp4346@keio.jp>
2024-01-12 04:07:35 +0900

    add states and hanle local storage

--- src/utils/states.ts
+++ src/utils/states.ts
@@ -2,15 +2,19 @@ import { atom } from "jotai";
 import { InferenceBox } from "./types";
 import { Label, defaultLabelsJaLabelMap } from "@/yolo/label";
 import { LoadingMessages } from "./consts";
+import { atomWithStorage } from "jotai/utils";
 
 // app state
 export const loadingMessageAtom = atom<LoadingMessages | undefined>(undefined);
 
 // object detection model inference
 export const inferenceCountAtom = atom<number>(0);
-export const topK = atom<number>(100);
-export const iouThreshold = atom<number>(0.35);
-export const scoreThreshold = atom<number>(0.2);
+export const topK = atomWithStorage<number>("agop_topK", 100);
+export const iouThreshold = atomWithStorage<number>("agop_iouThreshold", 0.35);
+export const scoreThreshold = atomWithStorage<number>(
+  "agop_scoreThreshold",
+  0.2
+);
 
 // detection result
 export const resultBoxesHistoryAtom = atom<InferenceBox[][]>([[]]);
@@ -27,3 +31,11 @@ export const currentCamerasAtom = atom<MediaDeviceInfo[]>([]);
 // prompt dialog state
 export const isPromptDialogOpenAtom = atom<boolean>(false);
 export const promptDialogMessageAtom = atom<string>("");
+export const llmResponseAtom = atom<string>("");
+
+// OpenAI API
+export const apiKeyAtom = atomWithStorage<string>("agop_apiKeyAtom", "");
+export const modelNameAtom = atomWithStorage<string>(
+  "agop_modelNameAtom",
+  "gpt-3.5-turbo-0301"
+);
